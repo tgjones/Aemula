90763a8ae236686d8eae22d08da71accc9024990
Tim Jones <tim@timjones.io>
2020-04-16 22:19:44 +0100

    Add placeholder imgui-based UI

---
+++ src/bin/emu/main.rs
@@ -0,0 +1,23 @@
+use imgui::*;
+
+mod support;
+
+fn main() {
+    let system = support::init(file!());
+    system.main_loop(move |_, ui| {
+        Window::new(im_str!("Hello world"))
+            .size([300.0, 110.0], Condition::FirstUseEver)
+            .build(ui, || {
+                ui.text(im_str!("Hello world!"));
+                ui.separator();
+                let mouse_pos = ui.io().mouse_pos;
+                ui.text(format!(
+                    "Mouse Position: ({:.1},{:.1})",
+                    mouse_pos[0], mouse_pos[1]
+                ));
+            });
+
+        let mut opened = false;
+        ui.show_demo_window(&mut opened);
+    });
+}
